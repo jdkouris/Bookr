d57634918ddb2c830c4507dd542e448e42692b56
John Kouris <jdkouris@gmail.com>
2019-09-24 10:45:23 -0700

    Added method to sign up a user

--- Bookr/Bookr/Model Controllers/APIController.swift
+++ Bookr/Bookr/Model Controllers/APIController.swift
@@ -24,4 +24,41 @@ enum NetworkError: Error {
 
 class APIController {
     
+    private let baseUrl = URL(string: "https://lambda-bookr.herokuapp.com/api")!
+    
+    // function for sign up
+    func signUp(with user: User, completion: @escaping (Error?) -> Void) {
+        
+        let signUpUrl = baseUrl.appendingPathComponent("auth/register")
+        
+        var request = URLRequest(url: signUpUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error.localizedDescription)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+    }
+    
 }
